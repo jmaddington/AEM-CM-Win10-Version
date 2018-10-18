8cdbbabff2297dc18686c277bb8d825c9b7b33d7
jmaddington <jm@jmaddington.com>
2018-10-18 14:45:41 -0400

    Iniital commit

---
+++ command.bat
@@ -0,0 +1,33 @@
+#Get min Windows version to test against
+$MinVersion = If ($env:MinVersion -eq $null ) {"1803"} Else {$env:MinVersion}
+
+#Get current Windows version
+$ver = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+
+#Current version to stdout
+#Exit 1 = error
+#Exit 0 = OK
+
+If ($ver -lt $MinVersion)
+{
+    Write-Host "<-Start Result->
+$ver
+<-End Result->"
+
+    Write-Host "<-Start Diagnostic->"
+
+    $sysinfo = systeminfo
+
+    Write-Host $sysinfo
+
+    Write-Host "<-End Diagnostic->"
+
+    Exit 1
+
+} Else {
+        Write-Host "<-Start Result->
+$ver
+<-End Result->"
+
+    Exit 0
+}
